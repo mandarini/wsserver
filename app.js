9122706838eed4ffb1e87e32ef1b1466f1b1f9db
Katerina <sk.katherine@gmail.com>
2018-05-10 00:02:18 +0300

    get port from env

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(5000, () => console.log(`Listening on 5000`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 
